0fea0d0a7af0356294055cac2c548bbc858d0f44
EpicPantalones <ethansorensen01@gmail.com>
2024-05-23 16:26:30 -0600

    This is the new testfile - works with rx_v3

--- source/rx_v2/testpad.py
+++ source/rx_v2/testpad.py
@@ -5,11 +5,16 @@ import time
 if __name__ == "__main__":
     
     new_rec = WlskReceiver("/home/enas2001/Documents/WLSK_tests/wireless-latency-shift-keying/config/wlsk-config-2-1.json",True,l.DEBUG)
-
+    
+    # new_rec._WlskReceiver__isrunning.value = True
+    # new_rec._send_wlsk_pings()
+    # time.sleep(3)
+    # new_rec.__isrunning.value = False
     new_rec.start_receiver()
     
     message = new_rec.block_until_message()
+    # time.sleep(7)
     
-    new_rec.stop_receiver()
+    # new_rec.stop_receiver()
     
-    print(message)
+    # print(message)

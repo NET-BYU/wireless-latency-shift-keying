5f8d7b0ade868ecf9613c80be54b6e5c105e8e23
EpicPantalones <ethansorensen01@gmail.com>
2024-06-06 09:27:22 -0600

    test file updates

--- source/rx_v2/testpad.py
+++ source/rx_v2/testpad.py
@@ -1,20 +1,30 @@
 from receiver_v3 import WlskReceiver
+from functools import partial
 import logging as l
+import signal
 import time
+import sys
+
+def signal_handler(processes, signal, frame):
+    print("Ctrl+C caught, terminating processes...")
+    for p in processes:
+        p.terminate()
+    for p in processes:
+        p.join()
+    print("All processes terminated.")
+    sys.exit(0)
 
 if __name__ == "__main__":
     
-    new_rec = WlskReceiver("/home/enas2001/Documents/WLSK_tests/wireless-latency-shift-keying/config/wlsk-config-2-1.json",True,l.DEBUG)
+    config_path = "/home/enas2001/Documents/WLSK_tests/wireless-latency-shift-keying/config/wlsk-config-2-1.json"
+    new_rec = WlskReceiver(config_path,True,l.DEBUG,doGraphs=False)
     
-    # new_rec._WlskReceiver__isrunning.value = True
-    # new_rec._send_wlsk_pings()
-    # time.sleep(3)
-    # new_rec.__isrunning.value = False
+    signal_handler_with_processes = partial(signal_handler, new_rec.processes)
+    signal.signal(signal.SIGINT, signal_handler_with_processes)
+    
     new_rec.start_receiver()
     
-    message = new_rec.block_until_message()
-    # time.sleep(7)
+    # message = new_rec.block_until_message()
     
     # new_rec.stop_receiver()
     
-    # print(message)

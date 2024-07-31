85b4b435423801705bb752aa78795792e0eb1937
Chris Kitras <chkitras@gmail.com>
2024-07-31 09:43:26 -0600

    Revert "prepping for new subbranch"
    This reverts commit 21b2e4476dfb42f4be8dd84f35285b3387304c3a.

--- source/rx_v2/receiver_v3.py
+++ source/rx_v2/receiver_v3.py
@@ -20,14 +20,7 @@ LOG_OPS = 17
 l.addLevelName(LOG_PKTS,"Log Packets")
 l.addLevelName(LOG_DMPS,"Log Data Dumps")
 l.addLevelName(LOG_OPS,"Log Operations")
-
-''' TODO:
- - add in the rx_timeout_limit to the state machine
- - talk to Chris about making multiple instances vs. multiple lines
- - clean up the init stuff
- - review the logging states and messages
- - wrap things in user-friendliness
-'''      
+        
 
 class WlskReceiver:
     '''
@@ -39,7 +32,7 @@ class WlskReceiver:
     VERSION = 2.1
         
     def __init__(self, config_path: string, log_to_console: bool=False,
-                 log_level: int=l.INFO, logfile: string=None, saveAllWindows: bool = False) -> None:
+                 log_level: int=l.INFO, logfile: string=None, doGraphs: bool = False) -> None:
         '''WLSK Receiver
         
         Keyword Arguments:
@@ -47,7 +40,7 @@ class WlskReceiver:
         - log_to_console: bool  -- log output messages to the console.
         - log_level: int        -- determine the log level WLSK should run at.
         - logfile: string       -- if given, WLSK will log outputs to the given file.
-        - saveAllWindows: bool        -- save graphs when processing windows. Slows the receiver considerably.
+        - doGraphs: bool        -- save graphs when processing windows. Slows the receiver considerably.
         '''
         self.l = l.getLogger(__name__)
         self.l.setLevel(log_level)
@@ -65,7 +58,7 @@ class WlskReceiver:
             self.l.addHandler(file_handler)
 
         # set up variables with required default values
-        self.saveAllWindows:    bool                = saveAllWindows
+        self.doGraphs:          bool                = doGraphs
         self.isInitalized:      bool                = False
         self.msg_output_dir:    string              = None
         self.DELETE_REQ:        int                 = -999
@@ -248,7 +241,7 @@ class WlskReceiver:
     
     def running(self) -> bool:
         '''returns true or false to indicate if the receiver is active.'''
-        return any([process.is_alive() for process in self.processes])
+        return any(process.is_alive() for process in self.processes)
     
     def _send_wlsk_pings(self) -> None:
         '''Pinger Process for WLSK
